2cae043471c1264e33b99984479972c397e4030d
GURSAL PRATHAMESH APPASAHEB <135983263+prathamesh424@users.noreply.github.com>
2024-03-04 00:41:49 +0530

    uploading Task of  03/03/2024

---
+++ removeNthNode19.java
@@ -0,0 +1,34 @@
+
+ public class removeNthNode19 {
+
+    public class ListNode {
+             int val;
+             ListNode next;
+             ListNode() {}
+             ListNode(int val) { this.val = val; }
+             ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+         }
+    
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+               ListNode fast = head , slow = head;
+               for (int i =0 ; i < n ; i++) {
+                   fast = fast.next ;
+                }
+                if (fast == null) {
+                    return head.next ;   
+                }
+                while (fast.next != null){
+                    fast = fast.next ;
+                    slow = slow.next ;
+                }
+                slow.next = slow.next.next ;
+                return head ; 
+        }
+            
+    public static void main(String[] args) {
+        
+    }
+
+     
+}
+
